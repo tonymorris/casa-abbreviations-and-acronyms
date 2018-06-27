e0c2208d1d9db68af7dd176aad9e3f4edef630ad
Tony Morris <tmorris@tmorris.net>
2018-06-27 15:32:36 +1000

    0.0.2

--- src/executable/Main.hs
+++ src/executable/Main.hs
@@ -2,8 +2,17 @@ module Main(
   main
 ) where
 
+import Data.Aviation.Casa.AbbreviationsAndAcronyms.Acronym
+import Data.Aviation.Casa.AbbreviationsAndAcronyms.Render
+import Data.Aviation.Casa.AbbreviationsAndAcronyms.Search
+import qualified Text.Fuzzy as Fuzzy(filter, score)
+import Text.Fuzzy(Fuzzy(Fuzzy))
+
 main ::
   IO ()
 main =
-  putStrLn "casa-abbreviations-and-acronyms"
-
+  let k = searchFuzzyName "BKN" "" "" False
+      kk :: [Acronym]; kk = (\(Fuzzy o r s) -> o) <$> k
+      l :: ConfigReader String; l = renderHeaderAcronyms kk
+      ll = runConfig l defaultConfig
+  in  putStrLn ll
---
+++ src/library/Data/Aviation/Casa/AbbreviationsAndAcronyms/Render.hs
@@ -0,0 +1,265 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Data.Aviation.Casa.AbbreviationsAndAcronyms.Render where
+
+import Control.Lens
+import Data.Aviation.Casa.AbbreviationsAndAcronyms.Acronym
+import Prelude
+import Data.List
+
+data Colour =
+  Colour
+    (String -> String) -- heading name
+    (String -> String) -- heading meaning
+    (String -> String) -- heading source
+    (String -> String) -- heading separator
+    (String -> String) -- acronym separator
+    (String -> String) -- acronym name
+    (String -> String) -- acronym meaning
+    (String -> String) -- acronym source
+
+noColour ::
+  Colour
+noColour =
+  Colour
+    id
+    id
+    id
+    id
+    id
+    id
+    id
+    id
+
+class HasColour a where
+  colour ::
+    Lens'
+      a
+      Colour
+  headingNameColour ::
+    Lens'
+      a
+      (String -> String)
+  {-# INLINE headingNameColour #-}
+  headingNameColour =
+    colour . headingNameColour
+  headingMeaningColour ::
+    Lens'
+      a
+      (String -> String)
+  {-# INLINE headingMeaningColour #-}
+  headingMeaningColour =
+    colour . headingMeaningColour
+  headingSourceColour ::
+    Lens'
+      a
+      (String -> String)
+  {-# INLINE headingSourceColour #-}
+  headingSourceColour =
+    colour . headingSourceColour
+  headingSeparatorColour ::
+    Lens'
+      a
+      (String -> String)
+  {-# INLINE headingSeparatorColour #-}
+  headingSeparatorColour =
+    colour . headingSeparatorColour
+  acronymSeparatorColour ::
+    Lens'
+      a
+      (String -> String)
+  {-# INLINE acronymSeparatorColour #-}
+  acronymSeparatorColour =
+    colour . acronymSeparatorColour
+  acronymNameColour ::
+    Lens'
+      a
+      (String -> String)
+  {-# INLINE acronymNameColour #-}
+  acronymNameColour =
+    colour . acronymNameColour
+  acronymMeaningColour ::
+    Lens'
+      a
+      (String -> String)
+  {-# INLINE acronymMeaningColour #-}
+  acronymMeaningColour =
+    colour . acronymMeaningColour
+  acronymSourceColour ::
+    Lens'
+      a
+      (String -> String)
+  {-# INLINE acronymSourceColour #-}
+  acronymSourceColour =
+    colour . acronymSourceColour
+
+instance HasColour Colour where
+  colour =
+    id
+
+data Config =
+  Config
+    Colour
+
+defaultConfig ::
+  Config
+defaultConfig =
+  Config
+    noColour
+    
+class HasConfig a where
+  config ::
+    Lens'
+      a
+      Config
+
+instance HasConfig Config where
+  config =
+    id
+
+instance HasColour Config where
+  colour =
+    config . colour
+
+newtype ConfigReader a =
+  ConfigReader
+    (Config -> a)
+
+instance ConfigReader a_aaRr ~ t_aaRq =>
+  Rewrapped (ConfigReader a_a86d) t_aaRq
+
+instance Wrapped (ConfigReader a_a86d) where
+  type Unwrapped (ConfigReader a_a86d) = Config -> a_a86d
+  _Wrapped' = (iso (\(ConfigReader x_aaRp) -> x_aaRp)) ConfigReader
+
+runConfig ::
+  ConfigReader a
+  -> Config
+  -> a
+runConfig (ConfigReader a) =
+  a
+
+instance Functor ConfigReader where
+  fmap f (ConfigReader g) =
+    ConfigReader (f . g)
+
+instance Applicative ConfigReader where
+  pure =
+    ConfigReader . pure
+  ConfigReader f <*> ConfigReader a =
+    ConfigReader (\x -> f x (a x))
+
+instance Monad ConfigReader where
+  return =
+    pure
+  ConfigReader a >>= f =
+    ConfigReader (\x -> runConfig (f (a x)) x)
+
+readColour ::
+  ConfigReader Colour
+readColour =
+  ConfigReader
+    (^. colour)
+
+readHeadingNameColour ::
+  ConfigReader (String -> String)
+readHeadingNameColour =
+  (^. headingNameColour) <$> readColour
+
+readHeadingMeaningColour ::
+  ConfigReader (String -> String)
+readHeadingMeaningColour =
+  (^. headingMeaningColour) <$> readColour
+
+readHeadingSourceColour ::
+  ConfigReader (String -> String)
+readHeadingSourceColour =
+  (^. headingSourceColour) <$> readColour
+
+readHeadingSeparatorColour ::
+  ConfigReader (String -> String)
+readHeadingSeparatorColour =
+  (^. headingSeparatorColour) <$> readColour
+
+readAcronymSeparatorColour ::
+  ConfigReader (String -> String)
+readAcronymSeparatorColour =
+  (^. acronymSeparatorColour) <$> readColour
+
+readAcronymNameColour ::
+  ConfigReader (String -> String)
+readAcronymNameColour =
+  (^. acronymNameColour) <$> readColour
+
+readAcronymMeaningColour ::
+  ConfigReader (String -> String)
+readAcronymMeaningColour =
+  (^. acronymMeaningColour) <$> readColour
+
+readAcronymSourceColour ::
+  ConfigReader (String -> String)
+readAcronymSourceColour =
+  (^. acronymSourceColour) <$> readColour
+
+renderHeader ::
+  ConfigReader String
+renderHeader =
+  do  hc <- readHeadingSeparatorColour
+      hn <- readHeadingNameColour
+      hm <- readHeadingMeaningColour
+      hs <- readHeadingSourceColour
+      pure . intercalate (hc " ") $
+        [
+          hn (mkN 8 "ACRONYM")
+        , hm (mkN 32 "MEANING")
+        , hs (mkN 15 "SOURCE")
+        ]
+
+renderAcronym ::
+  HasAcronym a =>
+  a
+  -> ConfigReader String
+renderAcronym a =
+  let name' =
+        a ^. name
+      meaning' =
+        a ^. meaning
+      source' =
+        a ^. source
+  in  do  sc <- readAcronymSeparatorColour
+          an <- readAcronymNameColour
+          am <- readAcronymMeaningColour
+          as <- readAcronymSourceColour
+          pure . intercalate (sc " ") $
+            [
+              an (mkN 8 name')
+            , am (mkN 32 meaning')
+            , as (mkN 15 source')
+            ]
+
+renderAcronyms ::
+  HasAcronym a =>
+  [a]
+  -> ConfigReader String
+renderAcronyms as =
+  concat <$> traverse renderAcronym as
+
+renderHeaderAcronyms ::
+  HasAcronym a =>
+  [a]
+  -> ConfigReader String
+renderHeaderAcronyms as =
+  do  h <- renderHeader
+      a <- renderAcronyms as
+      pure (h ++ "\n" ++ a)
+
+mkN ::
+  Int
+  -> String
+  -> String
+mkN n x =
+  let n' = n - length (take n x)
+  in  x ++ replicate n' ' '
